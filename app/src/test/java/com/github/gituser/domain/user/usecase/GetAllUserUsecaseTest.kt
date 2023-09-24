17bc6a28d5b5042a428694828d3aaccc2d26b024
Phanatagama <phanatagama@gmail.com>
2023-09-24 13:28:17 +0700

    feat(test): create GetAllUserUsecaseTest.kt

---
+++ app/src/test/java/com/github/gituser/domain/user/usecase/GetAllUserUsecaseTest.kt
@@ -0,0 +1,65 @@
+package com.github.gituser.domain.user.usecase
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.github.gituser.domain.user.entity.UserDetailEntity
+import com.github.gituser.domain.user.repository.UserRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.verify
+
+class GetAllUserUsecaseTest {
+    private lateinit var getAllUserUsecase: GetAllUserUsecase
+
+    @Mock
+    private lateinit var userRepository: UserRepository
+
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+        MockitoAnnotations.openMocks(this)
+        getAllUserUsecase = GetAllUserUsecase(userRepository)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
+        mainThreadSurrogate.close()
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `should emit list userDetailEntity`() = runTest {
+        val userDetailEntity = listOf(UserDetailEntity("joko"))
+        val userFlow = flow { emit(userDetailEntity) }
+
+        // arrange
+        `when`(userRepository.getAllUser()).thenReturn(userFlow)
+
+        // act
+        val result = getAllUserUsecase.invoke().first()
+
+        // assert
+        verify(userRepository).getAllUser()
+        assertEquals(userDetailEntity,result)
+    }
+
+}
