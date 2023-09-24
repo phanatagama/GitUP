42975efd909ca29af451dc74115f18fc2edeeb96
Phanatagama <phanatagama@gmail.com>
2023-09-24 13:23:14 +0700

    feat(ui): modify ListUserAdapter.kt

--- app/src/main/java/com/github/gituser/ui/main/ListUserAdapter.kt
+++ app/src/main/java/com/github/gituser/ui/main/ListUserAdapter.kt
@@ -1,17 +1,28 @@
 package com.github.gituser.ui.main
 
+import android.annotation.SuppressLint
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.github.gituser.databinding.ItemRowUserBinding
+import com.github.gituser.domain.user.entity.UserEntity
 
-class ListUserAdapter(private val listUser: ArrayList<Users>) : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
+class ListUserAdapter(private val listUser: MutableList<UserEntity>) : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
     private lateinit var onItemClickCallback: OnItemClickCallback
 
     fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
         this.onItemClickCallback = onItemClickCallback
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateList(data: List<UserEntity>){
+        listUser.clear()
+        listUser.addAll(data)
+        Log.d(TAG, "updateList: $listUser")
+        notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ListViewHolder {
@@ -20,23 +31,33 @@ class ListUserAdapter(private val listUser: ArrayList<Users>) : RecyclerView.Ada
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        val (username, avatar) = listUser[position]
-        Glide.with(holder.itemView.context)
-            .load(avatar)
-            .apply(RequestOptions().override(55, 55))
-            .into(holder.binding.imgItemPhoto)
-        holder.binding.tvItemName.text = username
-        holder.binding.tvItemUsername.text = username
-        holder.itemView.setOnClickListener {
-            onItemClickCallback.onItemClicked(listUser[holder.adapterPosition])
-        }
+        holder.bind(listUser[position])
     }
 
     override fun getItemCount(): Int = listUser.size
 
     interface OnItemClickCallback {
-        fun onItemClicked(data: Users)
+        fun onItemClicked(data: UserEntity)
     }
 
-    inner class ListViewHolder(var binding: ItemRowUserBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class ListViewHolder(private val binding: ItemRowUserBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(data: UserEntity){
+            Log.d(TAG, "bind: BINDING DATA INTO VIEW STARTED")
+            with(binding){
+            Glide.with(itemView.context)
+                .load(data.avatar)
+                .apply(RequestOptions().override(55, 55))
+                .into(imgItemPhoto)
+
+            tvItemName.text = data.username
+            tvItemUsername.text = data.username
+            root.setOnClickListener {
+                onItemClickCallback.onItemClicked(data)
+            }
+            }
+        }
+    }
+    companion object {
+        const val TAG ="LIST_USER_ADAPTER_TAG"
+    }
 }
