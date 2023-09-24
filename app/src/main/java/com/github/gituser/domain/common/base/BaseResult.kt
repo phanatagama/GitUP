0899b70f1e59f079e7e584aeb1ae62ec1039e441
Phanatagama <phanatagama@gmail.com>
2023-09-24 13:16:35 +0700

    feat(domain): Create BaseResult.kt

---
+++ app/src/main/java/com/github/gituser/domain/common/base/BaseResult.kt
@@ -0,0 +1,6 @@
+package com.github.gituser.domain.common.base
+
+sealed class BaseResult <out T : Any, out U : Any> {
+    data class Success <T: Any>(val data : T) : BaseResult<T, Nothing>()
+    data class Error <U : Any>(val err: U) : BaseResult<Nothing, U>()
+}
